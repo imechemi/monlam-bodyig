2b6160598090d8bc49e07fee0319d49bf446e389
tenzinchemi <chemi.tenzin@thoughtworks.com>
2019-09-27 00:08:37 +0530

    Installer script :)

--- mac/install.sh
+++ mac/install.sh
@@ -1,3 +1,6 @@
-echo "Tenzin chemi"
+curl -LO https://github.com/imechemi/monlam-bodyig/archive/master.zip
+open master.zip 
+cp monlam-bodyig-master/mac/fonts/* /Library/Fonts/
+cp monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
 
 
