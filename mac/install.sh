617d17c726f2907af545352dd2480608cf5dfa1a
tenzin chokden <chokdentenzin@gmail.com>
2023-08-05 16:51:30 +0530

    Update install.sh
    Changed font location from  system library ( /Library/Fonts) to user library ( ~/Library/Fonts )

--- mac/install.sh
+++ mac/install.sh
@@ -2,5 +2,5 @@ curl -LO https://github.com/imechemi/monlam-bodyig/archive/master.zip
 open master.zip 
 # Give persmission to execute the script
 chmod 744
-cp monlam-bodyig-master/mac/fonts/* /Library/Fonts/     
+cp monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
 cp monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
