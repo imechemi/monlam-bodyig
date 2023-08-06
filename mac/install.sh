6a479d6a3f0ad1522c28ea2fae410609d35331a8
tenzin chokden <chokdentenzin@gmail.com>
2023-08-06 22:19:12 +0530

    Update install.sh
    remove curl

--- mac/install.sh
+++ mac/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-curl -LO https://github.com/chokden/monlam-bodyig/archive/master.zip
-open master.zip 
-#create file 
+#curl -LO https://github.com/chokden/monlam-bodyig/archive/master.zip
+#open master.zip 
+#create file
+echo "Hello World !!!"
 touch hello
 rm hello
 cp -r ~/monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
