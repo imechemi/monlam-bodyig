f684540e25f493ec53e5ec19c3ab31a6225de90f
tenzin chokden <chokdentenzin@gmail.com>
2023-08-06 22:47:32 +0530

    Update install.sh
    sudo

--- mac/install.sh
+++ mac/install.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
-#curl -LO https://github.com/chokden/monlam-bodyig/archive/master.zip
-#open master.zip 
+curl -LO https://github.com/chokden/monlam-bodyig/archive/master.zip
+open master.zip 
 #create file
-echo "Hello World !!!"
-fonts="~/hello.txt"
-sudo touch chokden
-sudo cp chokden ~/Public
-touch $fonts
-touch "hello"
-rm hello
-cp -r ~/monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
-cp -r ~/monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
+
+sudo cp -r ~/monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
+sudo cp -r ~/monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
