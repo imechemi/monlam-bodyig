4c84efdad9b38f5bd53a4fdd1ea6f769ee5e72ff
tenzin chokden <chokdentenzin@gmail.com>
2023-08-06 21:20:06 +0530

    Update install.sh
    added path for file permission install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -1,6 +1,6 @@
 curl -LO https://github.com/imechemi/monlam-bodyig/archive/master.zip
 open master.zip 
 # Give persmission to execute the script
-chmod 744
+chmod 744 monlam-bodyig-master/mac/install.sh
 cp monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
 cp monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
