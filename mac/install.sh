0ead5ceaef46091f7876b85d771b84e2c54989de
tenzin chokden <chokdentenzin@gmail.com>
2023-08-06 21:25:40 +0530

    Update install.sh
    added home path ~/ to script.

--- mac/install.sh
+++ mac/install.sh
@@ -2,5 +2,5 @@ curl -LO https://github.com/imechemi/monlam-bodyig/archive/master.zip
 open master.zip 
 # Give persmission to execute the script
 chmod 744 monlam-bodyig-master/mac/install.sh
-cp monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
-cp monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
+cp -r ~/monlam-bodyig-master/mac/fonts/* ~/Library/Fonts/     
+cp -r ~/monlam-bodyig-master/mac/keyboard/* ~/Library/Keyboard\ Layouts/
